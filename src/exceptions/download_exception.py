1ba952d6f7a4b780c06c68d656a7d8326a418cc9
Danilo <danilogmoura@gmail.com>
2024-05-28 22:13:13 -0300

    Create download_exception.py

---
+++ src/exceptions/download_exception.py
@@ -0,0 +1,4 @@
+class DownloadException(Exception):
+    def __init__(self, message: str, original_exception: Exception = None):
+        super().__init__(message)
+        self.original_exception = original_exception
